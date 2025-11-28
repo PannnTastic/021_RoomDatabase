7e73c98c27612306eca0419b739dcb576fbbadf5
PannnTastic <pannntastic@gmail.com>
2025-11-28 15:40:16 +0700

    add PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/roomdatabase_021/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabase_021.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase_021.viewmodel.HomeViewModel
+import com.example.roomdatabase_021.repositori.AplikasiSiswa
+import com.example.roomdatabase_021.viewmodel.EntryViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
