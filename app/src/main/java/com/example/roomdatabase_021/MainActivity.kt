ef3b46f1ec179d161009bc0037f000019e695914
PannnTastic <pannntastic@gmail.com>
2025-11-28 17:31:33 +0700

    Memanggil fun SiswaApp ke MainActivity.kt

--- app/src/main/java/com/example/roomdatabase_021/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase_021/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomdatabase_021.ui.theme.RoomDatabase_021Theme
+import com.example.roomdatabase_021.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomDatabase_021Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
