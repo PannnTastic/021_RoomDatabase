13dd976c80566aba5c4cc2c2b34653955a8b4810
PannnTastic <pannntastic@gmail.com>
2025-11-28 15:25:39 +0700

    fill interface ContainerApp.kt

--- app/src/main/java/com/example/roomdatabase_021/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase_021/repositori/ContainerApp.kt
@@ -1,2 +1,29 @@
 package com.example.roomdatabase_021.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase_021.room.DatabaseSiswa
+
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) : ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa
+                .getDatabase(context)
+                .siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application(){
+    lateinit var container : ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+
+    }
+}
