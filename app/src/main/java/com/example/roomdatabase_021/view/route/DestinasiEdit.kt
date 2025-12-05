90efd3b2e957a278c39df032f1e86b1f70dceee7
PannnTastic <pannntastic@gmail.com>
2025-12-05 14:54:38 +0700

    fill DestinasiEdit.kt

--- app/src/main/java/com/example/roomdatabase_021/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/roomdatabase_021/view/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.roomdatabase_021.view.route
 
-object DestinasiEdit {
+import com.example.roomdatabase_021.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
