d2baf24e4f2269d3b0cb261749a526de9eac4057
PannnTastic <pannntastic@gmail.com>
2025-12-05 15:01:15 +0700

    initilize edit and detail in viewmodel provider

--- app/src/main/java/com/example/roomdatabase_021/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_021/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.roomdatabase_021.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase_021.viewmodel.HomeViewModel
 import com.example.roomdatabase_021.repositori.AplikasiSiswa
+import com.example.roomdatabase_021.viewmodel.DetailViewModel
+import com.example.roomdatabase_021.viewmodel.EditViewModel
 import com.example.roomdatabase_021.viewmodel.EntryViewModel
 
 object PenyediaViewModel {
@@ -15,6 +18,12 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
